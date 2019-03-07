32412ffdbf7655623968fe76bd662365b7ee8c25
Jamie Vella <jamievella96@gmail.com>
2019-03-07 15:37:13 +0100

    Added Sass transformer, react navigation and storybook packages

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styles from "./styles/global.scss";
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+let storybook = false;
+export default (__DEV__ && storybook) ? require('./storybook').default : App;
