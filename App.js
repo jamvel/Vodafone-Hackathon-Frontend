863975bfb3ea40467431bb3ad5521738a85d7269
Jamie Vella <jamievella96@gmail.com>
2019-03-09 09:39:28 +0100

    Added structure

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppContainer from "./components/AppContainer";
 import styles from "./styles/global.scss";
 
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      </View>
+    <React.Fragment>
+      <AppContainer />
+    </React.Fragment>
     );
   }
 }
---
+++ components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "native-base";
+export default class HeaderTransparent extends React.Component {
+  render() {
+    return (
+      <Container style={{backgroundColor: "#87cefa"}}>
+
+      </Container>
+    );
+  }
+}
